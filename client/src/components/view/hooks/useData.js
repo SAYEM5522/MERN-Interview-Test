bb0259d5ba1f063a36d124975b8ba6c9c486f814
Md <md1040582@gmail.com>
2023-12-15 11:35:42 +0600

    created hooks for data fetching

---
+++ client/src/components/view/hooks/useData.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const BASE_URL = 'https://mern-test-i7hp.onrender.com/drowing';
+
+const useData = (path,id) => {
+  const [drowing, setDrowing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchDrowingData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(id?`${BASE_URL}/${path}/${id}`:`${BASE_URL}/${path}`);
+      setDrowing(response.data);
+      setLoading(false);
+      setError(null);
+    } catch (error) {
+      console.error('Error fetching drowing data:', error);
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDrowingData();
+  }, []); // Empty dependency array means this effect runs once after the initial render
+
+  return { drowing, loading, error };
+};
+
+export default useData;
